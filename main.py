0eb0857267a9a48bc2f1cd469dcd29a272d4ebf8
Will Marshall <willcodymarshall@gmail.com>
2023-08-15 07:01:14 -0400

    TODONE

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
-import requests
-import json
 import argparse
-import os
-from pathlib import Path
+import re
 from collections import namedtuple
+from pathlib import Path
 
-
-users = {
-    "",
-}
+import requests
 
 ssh_key_types = {
     "sk-ecdsa-sha2-nistp256@openssh.com",
@@ -21,12 +16,30 @@ ssh_key_types = {
     "ssh-dss",
     "ssh-rsa",
 }
+key_type_regex = "|".join(f"(?:{re.escape(k)})" for k in ssh_key_types)
+authorized_key_regex = rf"^(?:(?P<options>.*)\s+)?(?P<keytype>{key_type_regex})\s+(?P<key>\S+)(?:\s+(?P<comment>.*))?$"
 
 
-AuthorizedKey = namedtuple("AuthorizedKey", field_names=["options", "keytype", "key", "comment"])
+AuthorizedKey = namedtuple(
+    "AuthorizedKey", field_names=["options", "keytype", "key", "comment"]
+)
 
-def parse_key(line:str) -> AuthorizedKey:
-    return AuthorizedKey("", "", "", "")
+
+def parse_key(line: str) -> AuthorizedKey:
+    match = re.match(authorized_key_regex, line)
+    if match:
+        return AuthorizedKey(
+            options=match["options"],
+            keytype=match["keytype"],
+            key=match["key"],
+            comment=match["comment"],
+        )
+    raise Exception(f"Failed to match {line} against {authorized_key_regex}")
+
+
+def serialize_key(key: AuthorizedKey) -> str:
+    return " ".join(f for f in key if f)
+
 
 def get_keys(username: str) -> set[str]:
     try:
@@ -38,33 +51,77 @@ def get_keys(username: str) -> set[str]:
         return set()
 
 
-def update_authorized_keys(sentinel_id: str, user_keys: dict[str, set[str]]):
-    authorized_keys_file = Path.home() / ".ssh/authorized_keys"
+def update_authorized_keys(
+    sentinel_id: str, authorized_keys_path: str, user_keys: dict[str, set[str]]
+):
+    authorized_keys_file = Path(authorized_keys_path)
     authorized_keys_file.parent.mkdir(exist_ok=True)
     authorized_keys_lines = []
     if authorized_keys_file.exists():
         with open(authorized_keys_file, "rt") as f:
             authorized_keys_lines = f.readlines()
-    authorized_keys = [parse_key(line) for line in authorized_keys_lines]
+    existing_authorized_keys = {}
+    for i, line in enumerate(authorized_keys_lines):
+        if line.startswith("#"):
+            continue
+        try:
+            existing_authorized_keys[i] = parse_key(line)
+        except Exception as e:
+            print(f"{e} on line {i}, ignoring line")
+
     # remove keys we've synced
-    authorized_keys = [key for key in authorized_keys if not key.comment.startswith(sentinel_id)]
-    authorized_keys +=
+    existing_key_lines = {i for i, key in existing_authorized_keys.items() if key.comment.startswith(sentinel_id)}
+    authorized_keys_lines = [
+        line for i, line in enumerate(authorized_keys_lines) if i not in existing_key_lines
+    ]
 
+    # add synced keys
+    for username, keys in user_keys.items():
+        sentinel_comment = f"{sentinel_id}-{username}"
+        for key in keys:
+            authorized_key = parse_key(key)._replace(comment=sentinel_comment)
+            authorized_keys_lines.append(serialize_key(authorized_key))
 
+    # write back lines
+    with open(authorized_keys_file, "wt") as f:
+        f.writelines(line + "\n" for line in authorized_keys_lines)
 
 
 def main():
     parser = argparse.ArgumentParser(
         prog="sync_gh_keys",
-        description="Keep a set of GitHub users public keys synced into authorized_keys"
+        description="Keep a set of GitHub users public keys synced into authorized_keys",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument("--sentinel-id", default=parser.prog)
-    _ = parser.parse_args()
+    parser.add_argument(
+        "-s",
+        "--sentinel-id",
+        help="prefix for comment lines marking keys synced by this program",
+        default=parser.prog,
+    )
+    parser.add_argument(
+        "-a",
+        "--authorized-keys-path",
+        help="path to authorized_keys file to sync",
+        default=(Path.home() / ".ssh/authorized_keys"),
+    )
+    parser.add_argument(
+        "-u",
+        "--users",
+        help="comma-separated list of users to sync, can be specified multiple times",
+        action="append",
+    )
+    args = parser.parse_args()
+    users: set[str] = set()
+    for u in args.users:
+        u: str
+        users |= set(u.split(","))
     keys_to_sync = {}
-    for username in users:
+    print(f"Syncing keys for {sorted(users)}")
+    for username in sorted(users):
         keys_to_sync[username] = get_keys(username)
 
-    update_authorized_keys(sentinel_id, keys_to_sync)
+    update_authorized_keys(args.sentinel_id, args.authorized_keys_path, keys_to_sync)
 
 
 if __name__ == "__main__":
